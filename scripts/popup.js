5e7bfe87729ea4b2fa6319c8c9b5e997943e3a58
Sơn Nguyễn <nbhson43@gmail.com>
2024-10-21 22:44:07 +0700

    update ui & format time

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const format = 'DD-MMM-YYYY hh:mm:ss A';
+    let format = localStorage.getItem('format') || 'DD-MMM-YYYY hh:mm:ss A'; // 12h
     const utcEl = document.getElementById('utc');
     const vietnamEl = document.getElementById('vietnam');
     const irelandEl = document.getElementById('ireland');
     const indiaEl = document.getElementById('india');
+
+    // config
+    const twentyFourFormat = document.getElementById('btnRadio24h');
+    const twelveFormat = document.getElementById('btnRadio12h');
+
+    twelveFormat.addEventListener('click', () => {
+        format = 'DD-MMM-YYYY hh:mm:ss A'; // 12h
+        localStorage.setItem('format', format)
+    })
+
+    twentyFourFormat.addEventListener('click', () => {
+        format = 'DD-MMM-YYYY HH:mm:ss'; // 24h
+        localStorage.setItem('format', format)
+    })
+
+    setRadioButtonFormat();
 
     setInterval(() => {
         const [uctDate, utcTime, utcPeriods] = moment.utc().format(format).split(" ");
@@ -19,17 +35,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function generateDateFormat(uctDate, utcTime, utcPeriods, isVietnam = false) {
         return `<div class="d-flex justify-content-center align-items-center w-100">
-                     <img
+                    <img
                         src="./images/date.svg"
                         alt
                         srcset
                         class="date me-2 mb-1"
                     >
                     <strong class="text-${isVietnam ? 'danger' : 'primary'} utc-date">${uctDate}</strong>
-                    <span class="badge text-bg-warning ms-3 d-flex align-items-center utc-time">
+                    <span class="badge text-bg-warning ms-3 d-flex align-items-center utc-time badge-custom">
                         ${utcTime}
-                        <span class="badge text-bg-${utcPeriods === 'PM' ? 'danger' : 'success'} ms-2">${utcPeriods}</span>
+                        ${utcPeriods ? `<span class="badge text-bg-${utcPeriods === 'PM' ? 'danger' : 'success'} ms-2">${utcPeriods}</span>` : ``}
                     </span>
                 </div>`;
     }
+
+    function setRadioButtonFormat() {
+        if (format === 'DD-MMM-YYYY HH:mm:ss') {
+            twentyFourFormat.checked = true;
+        } else {
+            twelveFormat.checked = true;
+        }
+    }
+
+
 });
